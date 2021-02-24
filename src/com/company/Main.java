02c51b06043ea5792af7057bd044726d2e628a1a
Mohd Fazal Mirza <abbasfazal9792@gmail.com>
2021-02-24 21:08:23 +0530

    Initial commit "hi here is my first commit"

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        boolean error=false;
+        do{
+            error=false;
+            try {
+                System.out.println("enter a number");
+                number = sc.nextInt();
+                int result=factorialOfNumber(number);
+                System.out.println(result);
+
+            }catch(InputMismatchException e){
+                System.out.println("Enter a integer Value");
+                error=true;
+                sc.nextLine();
+            }
+
+        }while(error);
+
+
+
+
+
+
+	//System.out.println("Hello World!");
+	//System.out.println("I Love Java");
+    }
+    static int factorialOfNumber(int n){
+        int factorial=1;
+        while(n>=1)
+        {
+            if(n==1) {
+                System.out.print(n + "=");
+                break;
+            }else {
+                System.out.print(n + "*");
+            }
+            factorial*=n;
+            n--;
+        }
+
+        return factorial;
+    }
+}
